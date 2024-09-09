9c87f38abc2b36ecfb23a8be2b7a48dd83c39fae
Jason Everett <jaseeverett@gmail.com>
2024-09-09 17:36:45 +1000

    Improvements to code and text.

--- utils-functions.R
+++ utils-functions.R
@@ -239,16 +239,16 @@ splnr_plot_featureRep <- function(df, category = NA,
      # guide = ggplot2::guide_legend(nrow = nr)
     ) +
     ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5, size = 16, colour = "black"),
-      axis.text.y = ggplot2::element_text(size = 16, colour = "black"),
+      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5, colour = "black"),
+      # axis.text.y = ggplot2::element_text(size = 16, colour = "black"),
       axis.title.x = ggplot2::element_blank(),
-      axis.title.y = ggplot2::element_text(size = 16),
+      # axis.title.y = ggplot2::element_text(size = 16),
       legend.title = ggplot2::element_blank(),
-      legend.text = ggplot2::element_text(size = 16),
+      # legend.text = ggplot2::element_text(size = 16),
      # legend.position.inside = c(0.5, 0.95),
       #legend.direction = "horizontal",
-      legend.background = ggplot2::element_rect(fill = "NA"),
-      title = ggplot2::element_text(size = 16)
+      legend.background = ggplot2::element_rect(fill = "NA")
+      # title = ggplot2::element_text(size = 16)
     )
 
   if (showTarget) {
