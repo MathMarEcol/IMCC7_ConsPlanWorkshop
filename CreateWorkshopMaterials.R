b2271f280515e39df57539abe09b30a7aaa4b995
Jason Everett <jaseeverett@gmail.com>
2024-10-11 20:38:08 +1100

    Update resources

--- CreateWorkshopMaterials.R
+++ CreateWorkshopMaterials.R
@@ -7,7 +7,7 @@ knitr::purl(input = "notes.qmd", output = "IMCC7_Code.R")
 # Render the files so the latest .html files are available
 quarto::quarto_render()
 
-# Update zip with new bits and pieces
+# Update zip with new bits and pieces and copy to docs folder
 utils::zip(zipfile = "docs/resources/IMCC7_ConsPlanWorkshop.zip",
            files = c("IMCC7_Code.R",
                      "Output",
@@ -15,5 +15,6 @@ utils::zip(zipfile = "docs/resources/IMCC7_ConsPlanWorkshop.zip",
                      "utils-functions.R",
                      "docs/notes.html",
                      "docs/data.html",
+                     "IMCC7_ConsPlanWorkshop.Rproj",
                      "Marxan_newMaPP_Tutorial_Galapagos_Oct2024.pdf"
                      ))
